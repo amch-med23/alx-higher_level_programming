72736c8ead9ff1ff0472ee18de21bbd982f89676
amch-med23 <skyied.med2023@gmail.com>
2023-09-05 16:16:02 +0100

    fixing a typo

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-str = "Less thans 6 and not 0"
+str = "Less than 6 and not 0"
 if number < 0:
     number = -1 * number
     last_digit = -1 * (number % 10)
