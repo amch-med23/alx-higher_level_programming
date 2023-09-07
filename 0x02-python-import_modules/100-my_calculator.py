cfcdecd1298c841f9f7cb2176413ff0336286a21
amch-med23 <skyied.med2023@gmail.com>
2023-09-07 18:30:38 +0100

    adding the file for the tast 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, mul, div, sub
+    args_len = (len(sys.argv) - 1)
+    if args_len != 3:
+        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
+    op = sys.argv[2]
+    ops = {'+': add, '-': sub, '/': div, '*': mul}
+    if op not in list(ops.keys()):
+        print("Unknown operator. Available oparators: +, -, * and /")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[op](a, b)))
