849dfa12d95f88527bcfcc0f81e0a12634302ed9
amch-med23 <skyied.med2023@gmail.com>
2023-09-07 18:32:34 +0100

    fixing probable typos in the error messages

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,12 +4,12 @@ if __name__ == "__main__":
     from calculator_1 import add, mul, div, sub
     args_len = (len(sys.argv) - 1)
     if args_len != 3:
-        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     op = sys.argv[2]
     ops = {'+': add, '-': sub, '/': div, '*': mul}
     if op not in list(ops.keys()):
-        print("Unknown operator. Available oparators: +, -, * and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
