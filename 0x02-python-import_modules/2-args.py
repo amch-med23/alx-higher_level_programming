770f1d12619674228253ee30a84835a2cafedf48
amch-med23 <skyied.med2023@gmail.com>
2023-09-07 17:01:27 +0100

    fixing the typo in case of one arguemnt is passed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,11 @@ if __name__ == "__main__":
     args_len = len(sys.argv) - 1
     if (args_len == 0):
         print("{} arguments.".format(args_len))
+    elif args_len == 1:
+        print("{} argument:".format(args_len))
     else:
-        i = 1
         print("{} arguments:".format(args_len))
-        while i <= args_len:
-            print("{}: {}".format(i, sys.argv[i]))
-            i += 1
+    i = 1
+    while i <= args_len:
+        print("{}: {}".format(i, sys.argv[i]))
+        i += 1
