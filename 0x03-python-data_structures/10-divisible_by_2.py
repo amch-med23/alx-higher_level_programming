4e374e7ac31437043ef29ddea737841e95ac59e9
amch-med23 <skyied.med2023@gmail.com>
2023-09-09 16:21:01 +0100

    fixing a typo in the function name

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
-def diivisible_by_2(my_list=[]):
+def divisible_by_2(my_list=[]):
     n_list = []
     for i in my_list:
         if i % 2 == 0:
             n_list.append(True)
         else:
             n_list.append(False)
-    return n_list
+    return (n_list)
