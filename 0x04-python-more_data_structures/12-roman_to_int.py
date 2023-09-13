22d5dcf703bbab0532637db49372e90e4c0c1f4a
amch-med23 <skyied.med2023@gmail.com>
2023-09-13 20:38:12 +0100

    fixing a typo in the file name

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    t = 0
+    dts = {'M': 1000, 'D': 500, 'C': 100, 'X': 10, 'L': 50, 'I': 1, 'V': 5}
+    for rom in reversed(roman_string):
+        ar = dts[rom]
+        t += ar if t < ar * 5 else - ar
+    return (t)
