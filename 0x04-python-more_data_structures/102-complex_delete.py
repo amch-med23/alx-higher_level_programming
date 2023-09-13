529a1600748c243213d95d4d06acc156db24f237
amch-med23 <skyied.med2023@gmail.com>
2023-09-13 20:13:43 +0100

    updating the code of the 102 to use the same value of key iin the del op

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,6 +4,6 @@ def complex_delete(a_dictionary, value):
     for k in a_dictionary:
         if a_dictionary[k] == value:
             key_to_del.append(k)
-        for i in key_to_del:
-            del a_dictionary[i]
+        for k in key_to_del:
+            del a_dictionary[k]
         return (a_dictionary)
