db9da7778a84449b8840fd29869bbeba45b81458
amch-med23 <skyied.med2023@gmail.com>
2023-09-25 19:36:09 +0100

    fixing a typo in a debug msg and changing the name of the newly generated list

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    _list = []
+    n_list = []
     for x in range(0, list_lenght):
         try:
             op = my_list_1[x] / my_list_2[x]
         except TypeError:
-            print("Wrong type")
+            print("wrong type")
             op = 0
         except ZeroDivisionError:
             print("division by 0")
@@ -14,5 +14,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             op = 0
         finally:
-            _list.append(op)
-    return (_list)
+            n_list.append(op)
+    return (n_list)
