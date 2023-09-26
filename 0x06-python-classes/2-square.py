076036cc71664c19e5ee27a34bb0312463a82203
amch-med23 <skyied.med2023@gmail.com>
2023-09-26 21:15:37 +0100

    fixing a typos in the code

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,9 +10,9 @@ class Square:
           TypeError: error indication a type in not integer
           ValueError: error indicationg is the size is less than 0
     """
-    def __int__(self, size=0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
-            raise TypeError('size must be integer')
+            raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
