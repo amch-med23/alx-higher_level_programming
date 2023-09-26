7990f5e454382fc100c4fe06c51f54e029b76274
amch-med23 <skyied.med2023@gmail.com>
2023-09-26 22:29:18 +0100

    adding the file 102

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" the Square module """
+
+
+def __init__(self, value=0):
+    """
+    creating the square
+    Args:
+        size: the size of the square
+    """
+    self.__size = size
+
+    @property
+    def size(self):
+        """ the property of size of the square
+        Raises:
+              TypeError: if size is not an integer
+              ValueError: if size is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """
+        getting the rea of a square
+        Return: returns the size of the square
+        """
+        return self.__size * self.__size
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __ge__(selfe, other):
+        return self.area() >= other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
