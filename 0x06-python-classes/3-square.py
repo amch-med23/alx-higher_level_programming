6df3b32cfe154728fb8441955b8e7efd68ca7250
amch-med23 <skyied.med2023@gmail.com>
2023-09-26 21:16:56 +0100

    fixing typos in the code

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -6,7 +6,7 @@
 class Square:
     """this square has a private instance attribute called size"""
 
-    def __int__(self, size=0):
+    def __init__(self, size=0):
         """
         initialization
         Args:
@@ -16,7 +16,7 @@ class Square:
             if size < 0:
                 raise ValueError('size must be >= 0')
             else:
-                selfe.__size = size
+                self.__size = size
         else:
             raise TypeError('size must be an integer')
 
