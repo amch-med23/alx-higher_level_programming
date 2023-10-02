a150f79d1b5777cb621d5256400fad13c413623f
amch-med23 <skyied.med2023@gmail.com>
2023-10-02 17:29:17 +0100

    adding the description to the __repr__

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -74,6 +74,7 @@ class Rectangle:
         return (rec)
 
     def __repr__(self):
+        """ returns a string representation of the rectangle """
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
 
     def __del__(self):
