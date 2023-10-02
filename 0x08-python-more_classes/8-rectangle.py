0b2cb1587966d2de868586ea816ea32328afb574
amch-med23 <skyied.med2023@gmail.com>
2023-10-02 17:31:42 +0100

    adding the function documentation to the bigger_ot_equal method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -84,6 +84,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """ returns the result of a comparision between two rects """
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
