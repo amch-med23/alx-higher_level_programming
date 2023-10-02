a64d1fe7ec87c22951b7649ee6822e24ceb5bbe2
amch-med23 <skyied.med2023@gmail.com>
2023-10-02 17:23:15 +0100

    fixing a typo in the area method name

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,12 +44,12 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    def arae(self):
+    def area(self):
         """ return the area of a rectangle """
         return (self.__width * self.__height)
 
     def perimeter(self):
         """ returns the perimeter of the rectangle """
         if self.__width == 0 or self.__height == 0:
-            return 0
+            return (0)
         return ((self.__width * 2) + (self.__height * 2))
