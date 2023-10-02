30306c1cb54b45c7f20e3c2a27e7533650dac42a
amch-med23 <skyied.med2023@gmail.com>
2023-10-02 17:04:40 +0100

    adhering to the doc rules

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+""" A calss that defines a rectangle """
 class Rectangle:
-    """
-    an empty class defines the rectangle
-"""
+    """  an empty class defines the rectangle """
     pass
