dcec9881f5676b03a972dadb4cffc1f3d7189cff
amch-med23 <skyied.med2023@gmail.com>
2023-10-10 16:29:48 -0600

    adding the file 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-""" this module loads, adds, and saves
-all the arguments as a json file """
+"""This module adds all arguments to a Python list and save them to a file."""
+
+
 import sys
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
     try:
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
