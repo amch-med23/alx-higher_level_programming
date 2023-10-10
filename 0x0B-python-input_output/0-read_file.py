805b63dcdf7fb23928eb5cd319f912bfeeaad455
amch-med23 <skyied.med2023@gmail.com>
2023-10-10 11:21:13 -0600

    fixing some styling issues in the first file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ this is a module to read and print the content of a file """
 
+
 def read_file(filename=""):
     """ this function reads and outputs a content of a text file """
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end='')
-
