88a754882c433e28e33064f58d1250003d5a4adb
amch-med23 <skyied.med2023@gmail.com>
2023-12-13 01:39:26 +0100

    fixing the spacing in the code.

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -3,10 +3,11 @@
  * Represents a rectangle class with width and height attribues
  */
 
-class Rectangle {
-    constructor (w, h){
-	this.width = w;
-	this.height = h;
-    }
-}
-module.exports = Rectangle;
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
