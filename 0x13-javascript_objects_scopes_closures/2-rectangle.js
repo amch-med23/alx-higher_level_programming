5baa5bd5172e87bb2bc8cf816ea1f0f3abbcdd65
amch-med23 <skyied.med2023@gmail.com>
2023-12-13 01:45:26 +0100

    updating the spacing issue for file 2 #1

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 /**
- * this checks the arameters provided
+ * Check the parameters provided
  */
 class Rectangle {
-    constructor (w, h){
-	if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0){
-	    this.width = w;
-	    this.height = h;
-	}    
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 module.exports = Rectangle;
