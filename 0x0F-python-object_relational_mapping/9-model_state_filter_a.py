b67a679b599a218217c3d49f4ffdbc9c169dea4d
amch-med23 <skyied.med2023@gmail.com>
2023-12-18 03:14:19 +0100

    update task 9 file

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""
-This script lists all State objects
-that contain the letter `a`
-from the database `hbtn_0e_6_usa`.
-"""
+"lists all the States that contain `a` in there name"
 
 from sys import argv
-from model_state import State, Base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sqlalchemy import create_engine, select
 
-if __name__ == "__main__":
-    """
-    Access to the database and get a state
-    from the database.
-    """
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    conn = engine.connect()
+    query = select(State).where(State.name.contains('a')).order_by(State.id)
+    states = conn.execute(query)
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+    for id, name in states:
+        print(f'{id}: {name}')
 
-    engine = create_engine(db_url)
-    Session = sessionmaker(bind=engine)
-
-    session = Session()
-
-    states = session.query(State).filter(State.name.contains('a'))
-    if states is not None:
-        for state in states:
-            print('{0}: {1}'.format(state.id, state.name))
+    engine.dispose()
