f20b41b82613dde0591ae7f6e1775bf6b95d88c5
amch-med23 <skyied.med2023@gmail.com>
2023-12-18 03:11:44 +0100

    updating task 4 file

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-"""
-This script lists all cities from
-the database `hbtn_0e_4_usa`.
-"""
-
-import MySQLdb as db
-from sys import argv
+"This script lists all cities"
 
 if __name__ == '__main__':
-    """
-    Access the database and get the cities
-    from the database.
-    """
+    import MySQLdb
+    from sys import argv
 
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
+    conn = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                           passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT c.id, c.name, s.name FROM cities c JOIN states s \
+                WHERE c.state_id = s.id ORDER BY c.id")
 
-    with db_connect.cursor() as db_cursor:
-        db_cursor.execute("SELECT cities.id, cities.name, states.name \
-                                FROM cities JOIN states ON cities.state_id \
-                                = states.id ORDER BY cities.id ASC")
-        rows_selected = db_cursor.fetchall()
+    cities = cur.fetchall()
+    for row in cities:
+        print(row)
 
-    if rows_selected is not None:
-        for row in rows_selected:
-            print(row)
+    cur.close()
+    conn.close()
