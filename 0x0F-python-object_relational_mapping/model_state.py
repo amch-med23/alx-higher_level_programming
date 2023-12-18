a52486467206046b4254cd178fe1e702b5fc89c9
amch-med23 <skyied.med2023@gmail.com>
2023-12-18 03:15:46 +0100

    Task 6: updating model_state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,23 +1,15 @@
 #!/usr/bin/python3
-"""
-This script defines a City class
-to work with MySQLAlchemy ORM.
-"""
-from model_state import Base, State
-from sqlalchemy import Column, Integer, String, ForeignKey
+"First state Model"
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
 
 
-class City(Base):
-    """City class
-    Attributes:
-        __tablename__ (str): The table name of the class
-        id (int): The id of the class
-        name (str): The name of the class
-        state_id (int): The state the city belongs to
-    """
+class State(Base):
+    "Declarative States table"
+    __tablename__ = 'states'
 
-    __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
